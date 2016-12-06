eb6512eaed475325aa4e704f9d9976862efc2ec0
Scott Taylor <scott@motel.is>
2016-12-06 17:34:43 -0600

    hey we don't need to test these things because they work already and these tests are very slow

--- apps/day_5/test/day_5_test.exs
+++ apps/day_5/test/day_5_test.exs
@@ -3,10 +3,12 @@ defmodule DoorTest do
   @moduletag timeout: 120000
   @input "cxdnnyjw"
 
+  @tag :skip
   test "part 1 result" do
     assert "f77a0e6e" == Door.hash(@input)
   end
 
+  @tag :skip
   test "part 2 result" do
     assert "999828ec" == Door.ordered_hash(@input)
   end
