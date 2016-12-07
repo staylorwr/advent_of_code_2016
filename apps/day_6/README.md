144dcf1b8b06c2ac5c3e91e7bd0d8d7e31e5fd6c
Scott Taylor <scott@motel.is>
2016-12-06 20:21:07 -0600

    day 6

---
+++ apps/day_6/README.md
@@ -0,0 +1,24 @@
+# Day6
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `day_6` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:day_6, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `day_6` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:day_6]]
+    end
+    ```
+
---
+++ apps/day_6/lib/day_6.ex
@@ -0,0 +1,42 @@
+defmodule Day6 do
+  def extract_max(lines) do
+    lines
+    |> extract
+    |> Enum.map(&Enum.max_by(&1, fn {_, count} -> count end))
+    |> present
+  end
+
+  def extract_min(lines) do
+    lines
+    |> extract
+    |> Enum.map(&Enum.min_by(&1, fn {_, count } -> count end))
+    |> present
+  end
+
+  defp extract(lines) do
+    lines
+    |> String.split()
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.reduce([], &extract_most_used_char/2)
+  end
+
+  defp present(data) do
+    data
+    |> Enum.map(&elem(&1, 0))
+    |> to_string
+  end
+
+  defp extract_most_used_char([], []), do: []
+  defp extract_most_used_char(list, []) do
+    acc =  [%{}]
+    |> Stream.cycle
+    |> Enum.take(length(list))
+
+    extract_most_used_char(list, acc)
+  end
+
+  defp extract_most_used_char([head_c|tail_c], [head_a|tail_a]) do
+    updated_map = Map.update(head_a, head_c, 1, &(&1+1))
+    [updated_map|extract_most_used_char(tail_c, tail_a)]
+  end
+end
